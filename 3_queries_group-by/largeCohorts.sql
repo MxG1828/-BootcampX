7680bd247185271343d75c68af5c5418f69af35d
Michael Xiang <yorkmike4@gmail.com>
2021-01-05 09:49:38 +0000

    3 queries group-by

---
+++ 3_queries_group-by/largeCohorts.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as cohort_name, COUNT (students.*) as student_count
+From students
+JOIN cohorts ON students.cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING COUNT (students.*) >= 18
+ORDER BY COUNT (students.*);
